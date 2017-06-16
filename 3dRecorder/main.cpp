d45b99c3b813a0b23a46e2a4cd8ff15ea0696988
cxnvcarol <cxnvcarol@gmail.com>
2017-06-16 16:20:21 +0200

    importing eds is ok

--- 3dRecorder/main.cpp
+++ 3dRecorder/main.cpp
@@ -1,6 +1,24 @@
 #include "d3dRecorder.h"
 #include <QtWidgets/QApplication>
 #include "WorkingSession.h"
+
+#include "../External/EDSDK/EDSDK.h"
+
+void testsEDSDK()
+{
+	EdsInitializeSDK();
+	EdsCameraListRef *list;
+	
+	EdsGetCameraList(list);
+	qDebug("QUIERO");
+
+	EdsUInt32 *count=0;
+	EdsError er=EdsGetChildCount(*list, count);
+	qDebug("bailar contigo");
+
+	EdsTerminateSDK();
+}
+
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
@@ -9,6 +27,10 @@ int main(int argc, char *argv[])
 	WorkingSession ws;
 
 	ws.dosomething();
+
+
+	testsEDSDK();
+
 
 	w.show();
 	return a.exec();
