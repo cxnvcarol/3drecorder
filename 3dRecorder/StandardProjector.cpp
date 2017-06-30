877d9da295812caa7d08adaf3cf988b43d925786
cxnvcarol <cxnvcarol@gmail.com>
2017-06-30 17:28:26 +0200

    good to go

--- 3dRecorder/StandardProjector.cpp
+++ 3dRecorder/StandardProjector.cpp
@@ -66,7 +66,6 @@ StandardProjector::StandardProjector()
 	scrollArea->setVisible(false);
 	setCentralWidget(scrollArea);
 
-
 	resize(QGuiApplication::primaryScreen()->availableSize() * 3 / 5);
 }
 
@@ -101,24 +100,8 @@ void StandardProjector::setImage(const QImage &newImage)
 
 	scrollArea->setVisible(true);
 
-	//if (!fitToWindowAct->isChecked())	//TODO Check here
+	//if (!fitToWindowAct->isChecked())//TODO Check!
 		imageLabel->adjustSize();
-}
-
-
-bool StandardProjector::saveFile(const QString &fileName)
-{
-	QImageWriter writer(fileName);
-
-	if (!writer.write(image)) {
-		QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
-			tr("Cannot write %1: %2")
-			.arg(QDir::toNativeSeparators(fileName)), writer.errorString());
-		return false;
-	}
-	const QString message = tr("Wrote \"%1\"").arg(QDir::toNativeSeparators(fileName));
-	statusBar()->showMessage(message);
-	return true;
 }
 
 
@@ -144,100 +127,13 @@ static void initializeImageFileDialog(QFileDialog &dialog, QFileDialog::AcceptMo
 		dialog.setDefaultSuffix("jpg");
 }
 
-void StandardProjector::open()
-{
-	QFileDialog dialog(this, tr("Open File"));
-	initializeImageFileDialog(dialog, QFileDialog::AcceptOpen);
-
-	while (dialog.exec() == QDialog::Accepted && !loadFile(dialog.selectedFiles().first())) {}
-}
-
-void StandardProjector::saveAs()
-{
-	QFileDialog dialog(this, tr("Save File As"));
-	initializeImageFileDialog(dialog, QFileDialog::AcceptSave);
-
-	while (dialog.exec() == QDialog::Accepted && !saveFile(dialog.selectedFiles().first())) {}
-}
-
-void StandardProjector::copy()
-{
-#ifndef QT_NO_CLIPBOARD
-	QGuiApplication::clipboard()->setImage(image);
-#endif // !QT_NO_CLIPBOARD
-}
-
-#ifndef QT_NO_CLIPBOARD
-static QImage clipboardImage()
-{
-	if (const QMimeData *mimeData = QGuiApplication::clipboard()->mimeData()) {
-		if (mimeData->hasImage()) {
-			const QImage image = qvariant_cast<QImage>(mimeData->imageData());
-			if (!image.isNull())
-				return image;
-		}
-	}
-	return QImage();
-}
-#endif // !QT_NO_CLIPBOARD
-
-void StandardProjector::paste()
-{
-#ifndef QT_NO_CLIPBOARD
-	const QImage newImage = clipboardImage();
-	if (newImage.isNull()) {
-		statusBar()->showMessage(tr("No image in clipboard"));
-	}
-	else {
-		setImage(newImage);
-		setWindowFilePath(QString());
-		const QString message = tr("Obtained image from clipboard, %1x%2, Depth: %3")
-			.arg(newImage.width()).arg(newImage.height()).arg(newImage.depth());
-		statusBar()->showMessage(message);
-	}
-#endif // !QT_NO_CLIPBOARD
-}
-
-void StandardProjector::zoomIn()
-{
-	scaleImage(1.25);
-}
-
-void StandardProjector::zoomOut()
-{
-	scaleImage(0.8);
-}
-
-void StandardProjector::normalSize()
-{
-	imageLabel->adjustSize();
-	scaleFactor = 1.0;
-}
 
 void StandardProjector::fitToWindow()
 {
-	bool fitToWindow = fitToWindowAct->isChecked();
-	scrollArea->setWidgetResizable(fitToWindow);
-	if (!fitToWindow)
-		normalSize();
-	updateActions();
-}
-
-void StandardProjector::about()
-{
-	QMessageBox::about(this, tr("About Image Viewer"),
-		tr("<p>The <b>Image Viewer</b> example shows how to combine QLabel "
-			"and QScrollArea to display an image. QLabel is typically used "
-			"for displaying a text, but it can also display an image. "
-			"QScrollArea provides a scrolling view around another widget. "
-			"If the child widget exceeds the size of the frame, QScrollArea "
-			"automatically provides scroll bars. </p><p>The example "
-			"demonstrates how QLabel's ability to scale its contents "
-			"(QLabel::scaledContents), and QScrollArea's ability to "
-			"automatically resize its contents "
-			"(QScrollArea::widgetResizable), can be used to implement "
-			"zooming and scaling features. </p><p>In addition the example "
-			"shows how to use QPainter to print an image.</p>"));
+	//bool fitToWindow = fitToWindowAct->isChecked();
+	scrollArea->setWidgetResizable(true);
+	//if (!fitToWindow)
+	//	normalSize();
 }
 
 void StandardProjector::showInFullProjection()
@@ -253,21 +149,15 @@ void StandardProjector::showInFullProjection()
 	//
 	this->loadFile("C:\\Users\\naranjo\\Pictures\\IMG_0001.JPG");
 	//this->loadFile("C:\\Users\\naranjo\\Pictures\\toy.png");
-	fitToWindowAct->setChecked(true);
+	//fitToWindowAct->setChecked(true);
 	this->fitToWindow();
 }
 
 
-
+/*
 void StandardProjector::createActions()
 {
 	QMenu *fileMenu = menuBar()->addMenu(tr("&File"));
-
-	QAction *openAct = fileMenu->addAction(tr("&Open..."), this, &StandardProjector::open);
-	openAct->setShortcut(QKeySequence::Open);
-
-	saveAsAct = fileMenu->addAction(tr("&Save As..."), this, &StandardProjector::saveAs);
-	saveAsAct->setEnabled(false);
 
 
 	fileMenu->addSeparator();
@@ -277,26 +167,8 @@ void StandardProjector::createActions()
 
 	QMenu *editMenu = menuBar()->addMenu(tr("&Edit"));
 
-	copyAct = editMenu->addAction(tr("&Copy"), this, &StandardProjector::copy);
-	copyAct->setShortcut(QKeySequence::Copy);
-	copyAct->setEnabled(false);
-
-	QAction *pasteAct = editMenu->addAction(tr("&Paste"), this, &StandardProjector::paste);
-	pasteAct->setShortcut(QKeySequence::Paste);
-
 	QMenu *viewMenu = menuBar()->addMenu(tr("&View"));
 
-	zoomInAct = viewMenu->addAction(tr("Zoom &In (25%)"), this, &StandardProjector::zoomIn);
-	zoomInAct->setShortcut(QKeySequence::ZoomIn);
-	zoomInAct->setEnabled(false);
-
-	zoomOutAct = viewMenu->addAction(tr("Zoom &Out (25%)"), this, &StandardProjector::zoomOut);
-	zoomOutAct->setShortcut(QKeySequence::ZoomOut);
-	zoomOutAct->setEnabled(false);
-
-	normalSizeAct = viewMenu->addAction(tr("&Normal Size"), this, &StandardProjector::normalSize);
-	normalSizeAct->setShortcut(tr("Ctrl+S"));
-	normalSizeAct->setEnabled(false);
 
 	viewMenu->addSeparator();
 
@@ -305,10 +177,6 @@ void StandardProjector::createActions()
 	fitToWindowAct->setCheckable(true);
 	fitToWindowAct->setShortcut(tr("Ctrl+F"));
 
-	QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
-
-	helpMenu->addAction(tr("&About"), this, &StandardProjector::about);
-	helpMenu->addAction(tr("About &Qt"), &QApplication::aboutQt);
 }
 
 void StandardProjector::updateActions()
@@ -319,6 +187,7 @@ void StandardProjector::updateActions()
 	zoomOutAct->setEnabled(!fitToWindowAct->isChecked());
 	normalSizeAct->setEnabled(!fitToWindowAct->isChecked());
 }
+*/
 
 void StandardProjector::scaleImage(double factor)
 {
@@ -329,8 +198,8 @@ void StandardProjector::scaleImage(double factor)
 	adjustScrollBar(scrollArea->horizontalScrollBar(), factor);
 	adjustScrollBar(scrollArea->verticalScrollBar(), factor);
 
-	zoomInAct->setEnabled(scaleFactor < 3.0);
-	zoomOutAct->setEnabled(scaleFactor > 0.333);
+	//zoomInAct->setEnabled(scaleFactor < 3.0);
+	//zoomOutAct->setEnabled(scaleFactor > 0.333);
 }
 
 void StandardProjector::adjustScrollBar(QScrollBar *scrollBar, double factor)
--- 3dRecorder/StandardProjector.h
+++ 3dRecorder/StandardProjector.h
@@ -72,23 +72,11 @@ public:
 	bool loadFile(const QString &);
 
 	private slots:
-	void open();
-	void saveAs();
-	void copy();
-	void paste();
-	void zoomIn();
-	void zoomOut();
-	void normalSize();
 	void fitToWindow();
-	void about();
 
 	
 
 private:
-	void createActions();
-	void createMenus();
-	void updateActions();
-	bool saveFile(const QString &fileName);
 	void setImage(const QImage &newImage);
 	void scaleImage(double factor);
 	void adjustScrollBar(QScrollBar *scrollBar, double factor);
@@ -98,13 +86,6 @@ private:
 	QScrollArea *scrollArea;
 	double scaleFactor;
 
-
-	QAction *saveAsAct;
-	QAction *copyAct;
-	QAction *zoomInAct;
-	QAction *zoomOutAct;
-	QAction *normalSizeAct;
-	QAction *fitToWindowAct;
 };
 
 #endif
